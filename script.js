c6c495dbce78d20bbffa8625f6896a5d9eebf085
saeed <me.coderr@gmail.com>
2023-06-11 22:43:46 +0530

    updated the code and design. to update : functioning filters, cards design with box-shadows and eventlisteners and the nav bar to be changed

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 
 /**
  * to update :
- * create a new card-container after every 12 cards.
- * the card-container will have display:none feature
- * every card-container will have only 4 rows, indexed : row-0, row-1, row-2, row-3
- * and 12 cards indexed card-0-0 ... card-1-3... card-2-6... card-3-9... card-3-11
+ * update the design on cards and buttons with box shadows and eventListeners and stuff
+ * make filters functioning
+ * nav bar converted to a filter menu emerging from the left side via a button
  */
 
+let APIurl = "https://kontests.net/api/v1/all";
+
 let cardNum = 0;
-let rowNum = -1;
+let rowNum = 0;
 let pageNum = 0;
+let count=0;
 
 let replaceAlert = (text) => {
     return new Promise((resolve, reject) => {
@@ -50,9 +52,8 @@ let recieveData = async () =>
 {
     let response;
     let data;
-    let url = "https://kontests.net/api/v1/all";
 
-    const request = new Request(url, {
+    const request = new Request(APIurl, {
         "method" : "GET"
     });
 
@@ -79,51 +80,222 @@ let create_appendCard = (dataObj, row) =>
 {
     let card = document.createElement("div");
     card.setAttribute("class", "card");
-    card.setAttribute("id", `card-${rowNum % 4}-${cardNum++ % 3}`);
-    card.innerText=`==${cardNum-1} ${rowNum}==`;
-    card.style.padding="5px";
+    card.setAttribute("id", `card-${pageNum}-${rowNum}-${++cardNum}`);
+
+    let imgContainer = document.createElement("div");
+    imgContainer.setAttribute("class", "img-container")
+
+    let img = document.createElement("img");
+    img.setAttribute("src", "https://img.freepik.com/premium-vector/web-development-coding-programming-futuristic-banner-computer-code-laptop_3482-5582.jpg");
+    img.setAttribute("class", "card-image");
+
+    let name = document.createElement("div");
+    name.setAttribute("class", "card-name");
+    name.innerText=dataObj.name;
+
+    let start = document.createElement("div");
+    start.setAttribute("class", "start");
+    start.innerHTML=`Date : ${dataObj.start_time.slice(0, 10)}<br>Time : ${dataObj.start_time.slice(11, 16)}`;
+
+    let link = document.createElement("a");
+    link.setAttribute("href", `${dataObj.url}`)
+
+    let siteName = document.createElement("div");
+    siteName.setAttribute("class", "siteName");
+    siteName.innerText = `${dataObj.site}`;
+
+    let button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.setAttribute("class", "goto-button");
+    button.innerText = `Register Here →`;
+
+    link.appendChild(button);
+    imgContainer.appendChild(img);
+
+    card.appendChild(imgContainer);
+    card.appendChild(name);
+    card.appendChild(start);
+    card.appendChild(siteName);
+    card.appendChild(link);
 
     row.appendChild(card);
+
+    let site = dataObj.site;
+
+    // if(site == "CodeForces" || site == "CodeForces::Gym" || site == "TopCoder" || site == "AtCoder" || site == "CS Academy" || site == "CodeChef" || site == "HackerRank" || site == "HackerEarth" || site == "LeetCode" || site == "Toph")
+
+    switch(site)
+    {
+        
+        case "CodeForces" : 
+            siteName.parentElement.style.backgroundColor = "lightgray";
+            break;
+
+        case "CodeForces::Gym" : 
+            siteName.parentElement.style.backgroundColor = "paleblue";
+            break;
+
+        case "CodeForces" : 
+            siteName.parentElement.style.backgroundColor = "mintgreen";
+            break;
+
+        case "TopCoder" : 
+            siteName.parentElement.style.backgroundColor = "beige";
+            break;
+
+        case "AtCoder" : 
+            siteName.parentElement.style.backgroundColor = "lightyellow";
+            break;
+
+        case "CS Academy" : 
+            siteName.parentElement.style.backgroundColor = "lightpink";
+            break;
+
+        case "CodeChef" : 
+            siteName.parentElement.style.backgroundColor = "lightcoral";
+            break;
+
+        case "HackerRank" : 
+            siteName.parentElement.style.backgroundColor = "lavender";
+            break;
+
+        case "HackerEarth" : 
+            siteName.parentElement.style.backgroundColor = "palegreen";
+            break;
+
+        case "LeetCode" : 
+            siteName.parentElement.style.backgroundColor = "lightskyblue";
+            break;
+        
+        case "Toph" : 
+            siteName.parentElement.style.backgroundColor = "softpeach";
+            break;
+    }
 
     return card;
 }
 
-let create_appendRow = (box) =>
+let create_appendRow = (page) =>
 {
     let cardRow = document.createElement("div");
     cardRow.setAttribute("class", "card-row");
-    cardRow.setAttribute("id", `row-${++rowNum}`);
+    cardRow.setAttribute("id", `row-${pageNum}-${++rowNum}`);
     cardRow.style.display="flex";
     cardRow.style.flexDirection="row";
 
-
-    box.appendChild(cardRow);
+    page.appendChild(cardRow);
 
     return cardRow;
 }
 
-let select_createRow = () => 
+let select_createRow = (page) => 
 {
     let row;
-
-    let box = document.getElementById("card-box")
-    let rowList = box.getElementsByClassName("card-row");
+    let rowList = page.getElementsByClassName("card-row");
     
-    if(cardNum % 3 === 0)
+    if(cardNum === 3 || cardNum === 0)
     {
-        row = create_appendRow(box);
+        cardNum = 0;
+        row = create_appendRow(page);
     }
     else
     {
-        row = rowList[rowNum];
+        row = rowList[rowNum-1];
     }
 
     return row;
 }
 
-let run = async () => 
+let create_appendPage = (box) =>
+{
+    let page = document.createElement("div");
+    page.setAttribute("class", "page");
+    page.setAttribute("id", `page-${++pageNum}`);
+
+    box.appendChild(page);
+
+    return page;
+}
+
+let select_createPage = (box) =>
+{
+    let page;
+
+    // if((cardNum === 2 && rowNum === 3) || (cardNum === 0 && rowNum === 0))
+    if((rowNum === 0 && cardNum === 0) || (rowNum === 4 && cardNum === 3))
+    {
+        rowNum = 0;
+        page = create_appendPage(box);
+    }
+    else
+    {
+        page = document.getElementById(`page-${pageNum}`);
+    }
+
+    return page;
+}
+
+let showPage = (atPage) =>
+{
+    let pages = document.getElementsByClassName("page");
+    atPage -= 1;    // atPage = atPage - 1 , in order to match the index in object `pages`
+
+    for( let i = 0; i < pages.length; i++)
+    {
+        if(i === atPage)
+        {
+            pages[i].style.display = "block";
+        }
+        else
+        {
+            pages[i].style.display = "none";
+        }
+    }
+}
+
+let setPage = (footer, atPage) =>
+{
+    let lastPageNum = Math.ceil(count/12);
+    let pageNumBox = footer.children[1];
+
+    if(lastPageNum === 1)
+    {
+        footer.style.visibility = "hidden";
+        return;
+    }
+
+    if(atPage < 1)                 // gives circular scrolling effect
+    {
+        atPage = lastPageNum;
+    }
+    else if(atPage > lastPageNum)
+    {
+        atPage = 1;
+    }
+    
+    // if(atPage < 1)              // prevents page backward turn when at first page
+    // {                           // prevents page forward turn when at last page
+    //     return 1;
+    // }
+    // else if(atPage > lastPageNum)
+    // {
+    //     return lastPageNum;
+    // }
+
+    pageNumBox.innerText = `Page ${atPage}`;
+
+    showPage(atPage);
+
+    return atPage;
+}
+
+let execute_main = async () => 
 {
     const data = await recieveData();
+    let footer = document.getElementsByTagName("footer")[0];
+    let box = document.getElementById("card-box");
+
+    let atPage = 1;
     let card;
     let row;
     let page;
@@ -135,11 +307,28 @@ let run = async () =>
             continue;
         }
 
+        count++;
 
-        row = select_createRow();
+        page = select_createPage(box);
+        row = select_createRow(page);
         card = create_appendCard(data[i], row);
-
     }
+
+    setPage(footer, atPage);
+
+    document.getElementById("pageBackward").addEventListener("click", (event) => 
+    {
+        let elem = event.target;
+        atPage = setPage(footer, --atPage);
+    });
+
+    document.getElementById("pageForward").addEventListener("click", (event) => 
+    {
+        let elem = event.target;
+        atPage = setPage(footer, ++atPage);
+    });
+
+    console.log(data);
 }
 
-// run();
+execute_main();

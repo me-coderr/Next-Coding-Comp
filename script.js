96d73471dbdcd8ea80d5a16ce2ff70576d7e066b
Saeed Ahmed <me.coderr@gmail.com>
2023-06-14 12:20:42 +0530

    Merging update branch with main (#1)
    Joining the update and main branch

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 
 /**
  * to update :
- * update the design on cards and buttons with box shadows and eventListeners and stuff
  * make filters functioning
  * nav bar converted to a filter menu emerging from the left side via a button
  */
 
 let APIurl = "https://kontests.net/api/v1/all";
 
-let cardNum = 0;
-let rowNum = 0;
-let pageNum = 0;
-let count=0;
+let cardNum;
+let rowNum;
+let pageNum;
+let count;
 
 let replaceAlert = (text) => {
     return new Promise((resolve, reject) => {
-        let div=document.createElement("div");
-        div.setAttribute("class", "alert");
-        div.innerHTML=`<p>${text}<p>`;
-        document.body.appendChild(div);
+        // let alertContainer = document.createElement("div");
+        // alertContainer.setAttribute("class", "alertContainer");
+
+        let alertBox=document.createElement("div");
+        alertBox.setAttribute("class", "alert");
+        alertBox.innerHTML=`<p>${text}<p>`;
 
         let okButton = document.createElement("button");
         okButton.setAttribute("type", "button");
         okButton.setAttribute("class", "okButton");
         okButton.innerText="Ok";
-        div.appendChild(okButton);
+
+        alertBox.appendChild(okButton);
+        // alertContainer.appendChild(alertBox);
+        document.getElementsByTagName("body")[0].appendChild(alertBox);
 
         okButton.focus();
 
         okButton.addEventListener("click", (e) => {
-            div.remove();
+            alertBox.remove();
+            // alertContainer.remove();
             resolve("Clicked ok button");
         });
 
+        // alertContainer.addEventListener("click", (e) => {
         document.addEventListener("click", (e) => {
-            div.remove();
+            alertBox.remove();
+            // alertContainer.remove();
             resolve("Clicked outside alert box");
         });
     });
@@ -76,7 +83,7 @@ let recieveData = async () =>
     }
 }
 
-let create_appendCard = (dataObj, row) => 
+let create_appendCard = (dataObj, row, filterClicked) => 
 {
     let card = document.createElement("div");
     card.setAttribute("class", "card");
@@ -97,81 +104,136 @@ let create_appendCard = (dataObj, row) =>
     start.setAttribute("class", "start");
     start.innerHTML=`Date : ${dataObj.start_time.slice(0, 10)}<br>Time : ${dataObj.start_time.slice(11, 16)}`;
 
-    let link = document.createElement("a");
-    link.setAttribute("href", `${dataObj.url}`)
-
     let siteName = document.createElement("div");
     siteName.setAttribute("class", "siteName");
     siteName.innerText = `${dataObj.site}`;
 
+    let linkButton = document.createElement("a");
+    linkButton.setAttribute("href", `${dataObj.url}`);
+    linkButton.setAttribute("target", `_blank`);
+    linkButton.setAttribute("rel", `noopener noreferrer`);
+  
     let button = document.createElement("button");
     button.setAttribute("type", "button");
     button.setAttribute("class", "goto-button");
     button.innerText = `Register Here →`;
 
-    link.appendChild(button);
+    linkButton.appendChild(button);
     imgContainer.appendChild(img);
 
     card.appendChild(imgContainer);
     card.appendChild(name);
     card.appendChild(start);
     card.appendChild(siteName);
-    card.appendChild(link);
+    card.appendChild(linkButton);
 
     row.appendChild(card);
 
-    let site = dataObj.site;
+    let site = dataObj.site;;
 
-    // if(site == "CodeForces" || site == "CodeForces::Gym" || site == "TopCoder" || site == "AtCoder" || site == "CS Academy" || site == "CodeChef" || site == "HackerRank" || site == "HackerEarth" || site == "LeetCode" || site == "Toph")
 
-    switch(site)
+    if(site == undefined)
     {
+        site = filterClicked.id;
+        switch(site)
+        {
+            case "codeforces" : 
+                card.style.backgroundColor = "lightgray";
+                siteName.innerText = "CodeForces"
+                break;
+
+            case "codeforces_gym" : 
+                card.style.backgroundColor = "paleblue";
+                siteName.innerText = "CodeForces::Gym"
+                break;
+
+            case "top_coder" : 
+                card.style.backgroundColor = "beige";
+                siteName.innerText = "TopCoder"
+                break;
+
+            case "at_coder" : 
+                card.style.backgroundColor = "lightyellow";
+                siteName.innerText = "AtCoder"
+                break;
+
+            case "cs_academy" : 
+                card.style.backgroundColor = "lightpink";
+                siteName.innerText = "CS Academy"
+                break;
+
+            case "code_chef" : 
+                card.style.backgroundColor = "lightcoral";
+                siteName.innerText = "CodeChef"
+                break;
+
+            case "hacker_rank" : 
+                card.style.backgroundColor = "lavender";
+                siteName.innerText = "HackerRank"
+                break;
+
+            case "hacker_earth" : 
+                card.style.backgroundColor = "palegreen";
+                siteName.innerText = "HackerEarth"
+                break;
+
+            case "leet_code" : 
+                card.style.backgroundColor = "lightskyblue";
+                siteName.innerText = "LeetCode"
+                break;
+        }
         
-        case "CodeForces" : 
-            siteName.parentElement.style.backgroundColor = "lightgray";
-            break;
-
-        case "CodeForces::Gym" : 
-            siteName.parentElement.style.backgroundColor = "paleblue";
-            break;
-
-        case "CodeForces" : 
-            siteName.parentElement.style.backgroundColor = "mintgreen";
-            break;
-
-        case "TopCoder" : 
-            siteName.parentElement.style.backgroundColor = "beige";
-            break;
-
-        case "AtCoder" : 
-            siteName.parentElement.style.backgroundColor = "lightyellow";
-            break;
-
-        case "CS Academy" : 
-            siteName.parentElement.style.backgroundColor = "lightpink";
-            break;
-
-        case "CodeChef" : 
-            siteName.parentElement.style.backgroundColor = "lightcoral";
-            break;
-
-        case "HackerRank" : 
-            siteName.parentElement.style.backgroundColor = "lavender";
-            break;
-
-        case "HackerEarth" : 
-            siteName.parentElement.style.backgroundColor = "palegreen";
-            break;
-
-        case "LeetCode" : 
-            siteName.parentElement.style.backgroundColor = "lightskyblue";
-            break;
-        
-        case "Toph" : 
-            siteName.parentElement.style.backgroundColor = "softpeach";
-            break;
     }
+    else
+    {
+        switch(site)
+        {
+            
+            case "CodeForces" : 
+                card.style.backgroundColor = "lightgray";
+                break;
 
+            case "CodeForces::Gym" : 
+                card.style.backgroundColor = "paleblue";
+                break;
+
+            case "CodeForces" : 
+                card.style.backgroundColor = "mintgreen";
+                break;
+
+            case "TopCoder" : 
+                card.style.backgroundColor = "beige";
+                break;
+
+            case "AtCoder" : 
+                card.style.backgroundColor = "lightyellow";
+                break;
+
+            case "CS Academy" : 
+                card.style.backgroundColor = "lightpink";
+                break;
+
+            case "CodeChef" : 
+                card.style.backgroundColor = "lightcoral";
+                break;
+
+            case "HackerRank" : 
+                card.style.backgroundColor = "lavender";
+                break;
+
+            case "HackerEarth" : 
+                card.style.backgroundColor = "palegreen";
+                break;
+
+            case "LeetCode" : 
+                card.style.backgroundColor = "lightskyblue";
+                break;
+            
+            case "Toph" : 
+                card.style.backgroundColor = "softpeach";
+                break;
+        }
+    }
     return card;
 }
 
@@ -222,7 +284,7 @@ let select_createPage = (box) =>
     let page;
 
     // if((cardNum === 2 && rowNum === 3) || (cardNum === 0 && rowNum === 0))
-    if((rowNum === 0 && cardNum === 0) || (rowNum === 4 && cardNum === 3))
+    if((rowNum === 0 && cardNum === 0) || (rowNum === 3 && cardNum === 3))
     {
         rowNum = 0;
         page = create_appendPage(box);
@@ -258,29 +320,30 @@ let setPage = (footer, atPage) =>
     let lastPageNum = Math.ceil(count/12);
     let pageNumBox = footer.children[1];
 
-    if(lastPageNum === 1)
+    if(lastPageNum <= 1)
     {
-        footer.style.visibility = "hidden";
         return;
     }
 
-    if(atPage < 1)                 // gives circular scrolling effect
-    {
-        atPage = lastPageNum;
-    }
-    else if(atPage > lastPageNum)
-    {
-        atPage = 1;
-    }
-    
-    // if(atPage < 1)              // prevents page backward turn when at first page
-    // {                           // prevents page forward turn when at last page
-    //     return 1;
+    footer.style.visibility = "visible";
+
+    // if(atPage < 1)                 // gives circular scrolling effect
+    // {
+    //     atPage = lastPageNum;
     // }
     // else if(atPage > lastPageNum)
     // {
-    //     return lastPageNum;
+    //     atPage = 1;
     // }
+    
+    if(atPage < 1)              // prevents page backward turn when at first page
+    {                           // prevents page forward turn when at last page
+        return 1;
+    }
+    else if(atPage > lastPageNum)
+    {
+        return lastPageNum;
+    }
 
     pageNumBox.innerText = `Page ${atPage}`;
 
@@ -289,11 +352,56 @@ let setPage = (footer, atPage) =>
     return atPage;
 }
 
-let execute_main = async () => 
+function filterClickHandler(event)
 {
-    const data = await recieveData();
+    APIurl = `https://kontests.net/api/v1/${event.target.id}`;
+    execute_main(event.target);
+}
+
+let applyFilters = () =>
+{
+    let elems = document.getElementsByClassName("filterType");
+
+    Array.from(elems).forEach((filter) => 
+    {
+        filter.addEventListener("click", filterClickHandler);
+
+        filter.addEventListener("dblclick", (event) => 
+        {
+            event.target.removeEventListener("click", filterClickHandler);
+            setTimeout(() =>
+            {
+                event.target.addEventListener("click", filterClickHandler);
+            }, 1000);
+        });
+    });
+}
+
+let hideAll = () =>
+{
+    let cardPages = document.getElementsByClassName("page");
+
+    Array.from(cardPages).forEach((page) =>
+    {
+        page.remove();
+    });
+}
+
+let execute_main = async (filterClicked) => 
+{
     let footer = document.getElementsByTagName("footer")[0];
     let box = document.getElementById("card-box");
+    
+    footer.style.visibility = "hidden";
+
+    cardNum=0;
+    rowNum=0;
+    pageNum=0;
+    count=0;
+
+    hideAll();
+
+    const data = await recieveData();
 
     let atPage = 1;
     let card;
@@ -311,7 +419,14 @@ let execute_main = async () =>
 
         page = select_createPage(box);
         row = select_createRow(page);
-        card = create_appendCard(data[i], row);
+        card = create_appendCard(data[i], row, filterClicked);
+    }
+
+    if(count === 0)
+    {
+        replaceAlert("For the time-being, there are no contests under the category : <br>" + filterClicked.getAttribute("name"));
+        setPage(footer, atPage);
+        return;
     }
 
     setPage(footer, atPage);
@@ -328,7 +443,7 @@ let execute_main = async () =>
         atPage = setPage(footer, ++atPage);
     });
 
-    console.log(data);
+    applyFilters();
 }
 
 execute_main();
